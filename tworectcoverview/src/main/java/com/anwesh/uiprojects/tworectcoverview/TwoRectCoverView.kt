a684926d33a2858f609eca54733a98679983a4b9
Anwesh43 <anweshthecool0@gmail.com>
2019-04-21 11:02:14 +0530

    adding TRCNode class

--- tworectcoverview/src/main/java/com/anwesh/uiprojects/tworectcoverview/TwoRectCoverView.kt
+++ tworectcoverview/src/main/java/com/anwesh/uiprojects/tworectcoverview/TwoRectCoverView.kt
@@ -126,4 +126,48 @@ class TwoRectCoverView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TRCNode(var i : Int, val state : State = State()) {
+
+        private var prev : TRCNode? = null
+        private var next : TRCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TRCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTRCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TRCNode {
+            var curr : TRCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
