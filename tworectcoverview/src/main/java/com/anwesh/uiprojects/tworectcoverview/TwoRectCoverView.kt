0ca6f02b063a3a5a960142d0f3b70c340e1ae8a6
Anwesh43 <anweshthecool0@gmail.com>
2019-04-21 10:54:27 +0530

    adding TwoRectCoverView class

--- tworectcoverview/src/main/java/com/anwesh/uiprojects/tworectcoverview/TwoRectCoverView.kt
+++ tworectcoverview/src/main/java/com/anwesh/uiprojects/tworectcoverview/TwoRectCoverView.kt
@@ -61,3 +61,21 @@ fun Canvas.drawTRCNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class TwoRectCoverView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
