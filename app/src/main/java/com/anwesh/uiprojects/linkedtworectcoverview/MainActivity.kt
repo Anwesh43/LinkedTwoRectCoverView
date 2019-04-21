86f4222c30fd9277d9056bb6c10898262715b9d3
Anwesh43 <anweshthecool0@gmail.com>
2019-04-21 11:19:15 +0530

    adding TwoRectCircleView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtworectcoverview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtworectcoverview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtworectcoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.tworectcoverview.TwoRectCoverView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TwoRectCoverView.create(this)
     }
 }
