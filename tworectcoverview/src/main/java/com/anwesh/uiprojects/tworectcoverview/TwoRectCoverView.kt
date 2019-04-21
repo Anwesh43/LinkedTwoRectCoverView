3fa68e9a2ea77e0b443315cc995fe9ea6ebb5608
Anwesh43 <anweshthecool0@gmail.com>
2019-04-21 11:07:19 +0530

    adding TwoRectCover class

--- tworectcoverview/src/main/java/com/anwesh/uiprojects/tworectcoverview/TwoRectCoverView.kt
+++ tworectcoverview/src/main/java/com/anwesh/uiprojects/tworectcoverview/TwoRectCoverView.kt
@@ -170,4 +170,28 @@ class TwoRectCoverView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TwoRectCover(var i : Int) {
+
+        private val root : TRCNode = TRCNode(0)
+        private var curr : TRCNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
