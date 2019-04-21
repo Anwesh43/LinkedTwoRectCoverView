cd623e170cc9773e59761c59363b56c89c03348a
Anwesh43 <anweshthecool0@gmail.com>
2019-04-21 11:23:15 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedtworectcoverview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtworectcoverview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedtworectcoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.tworectcoverview.TwoRectCoverView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TwoRectCoverView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
