697d1471018fe65aae1d9332c34f3888f0e62d90
Anwesh43 <anweshthecool0@gmail.com>
2019-04-21 10:33:26 +0530

    adding Int and Float extension methods

--- tworectcoverview/src/main/java/com/anwesh/uiprojects/tworectcoverview/TwoRectCoverView.kt
+++ tworectcoverview/src/main/java/com/anwesh/uiprojects/tworectcoverview/TwoRectCoverView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.tworectcoverview
 /**
  * Created by anweshmishra on 21/04/19.
  */
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val rects : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
